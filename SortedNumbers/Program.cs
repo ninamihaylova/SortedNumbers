0eb64b057521bfd184a9d1767713ac1704061d77
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-14 22:38:56 +0300

    Initial commit

---
+++ SortedNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿class SortedNumbers
+{
+    public static void Main(string[] args)
+    {
+        int number1 = int.Parse(Console.ReadLine());
+        int number2 = int.Parse(Console.ReadLine());
+        int number3 = int.Parse(Console.ReadLine());
+
+
+        if (number1 < number2 && number2 < number3)
+        {
+            Console.WriteLine("Ascending");
+        }
+        else if (number1>number2 && number2>number3) 
+        {
+            Console.WriteLine("Descending");
+        }
+        else
+        {
+            Console.WriteLine("Not sorted");
+        }
+    }
+}
